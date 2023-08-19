ef05903a98daf6329cd5b8cd4766f59a2f6f6763
viirgess <viirgess@gmail.com>
2023-08-19 20:41:41 +0300

    adding bloc cubit // no done

--- lib/menu/components/menu_card_items/card_items_modal_sheet.dart
+++ lib/menu/components/menu_card_items/card_items_modal_sheet.dart
@@ -1,9 +1,11 @@
 import 'dart:ui';
 
 import 'package:app/menu/components/menu_card_items/card_item_checkout.dart';
+import 'package:app/menu/cubit/add_item_to_busket/add_item_to_busket_cubit.dart';
 import 'package:app/utils/color_source.dart';
 import 'package:app/utils/text_style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'menu_footer_item.dart';
 
@@ -27,36 +29,77 @@ class CardModalSheet extends StatelessWidget {
             ),
           ],
         ),
-        child: FractionallySizedBox(
-          heightFactor: 0.82,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 25, top: 20, bottom: 20),
-                child: Text(
-                  'My Card',
-                  style: TextStyleApp.height20.copyWith(
-                    fontWeight: FontWeight.bold,
-                    color: ColorSourceApp.black,
-                    fontSize: 20,
-                  ),
+        child: BlocProvider<AddItemToBusketCubit>(
+          create: (context) => AddItemToBusketCubit(),
+          child: BlocBuilder<AddItemToBusketCubit, AddItemToBusketState>(
+            builder: (context, state) {
+              return FractionallySizedBox(
+                heightFactor: 0.82,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 25, top: 20, bottom: 20),
+                      child: Text(
+                        'My Card',
+                        style: TextStyleApp.height20.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: ColorSourceApp.black,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: context
+                            .read<AddItemToBusketCubit>()
+                            .cartItems
+                            .length,
+                        itemBuilder: (context, index) {
+                          final selectedItem = context
+                              .read<AddItemToBusketCubit>()
+                              .cartItems[index];
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12),
+                            child: CardItemCheckout(
+                              title: selectedItem.title,
+                              imagePath: selectedItem.imagePath,
+                              price: selectedItem.price,
+                              imageColor: selectedItem.imageColor,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: context
+                            .read<AddItemToBusketCubit>()
+                            .cartItems
+                            .length,
+                        itemBuilder: (context, index) {
+                          final selectedItem = context
+                              .read<AddItemToBusketCubit>()
+                              .cartItems[index];
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12),
+                            child: CardItemCheckout(
+                              title: selectedItem.title,
+                              imagePath: selectedItem.imagePath,
+                              price: selectedItem.price,
+                              imageColor: selectedItem.imageColor,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    const Spacer(),
+                    const MenuItemFooter(),
+                  ],
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 12,
-                ),
-                child: CardItemCheckout(
-                  imagePath: 'images/blue_donuts.png',
-                  title: 'SPRINKLE-TOPPED BLUE CREAM DREAM',
-                  price: '\$12.99',
-                  imageColor: ColorSourceApp.lightBlue,
-                ),
-              ),
-              const Spacer(),
-              const MenuItemFooter(),
-            ],
+              );
+            },
           ),
         ),
       ),
--- lib/menu/widget/button_busket.dart
+++ lib/menu/widget/button_busket.dart
@@ -1,5 +1,7 @@
+import 'package:app/menu/cubit/add_item_to_busket/add_item_to_busket_cubit.dart';
 import 'package:app/menu/menu_page_main.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../pages/order_check.dart';
 import '../../utils/color_source.dart';
@@ -11,68 +13,73 @@ class ButtonAddToBusketItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 45,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        border: Border.all(
-          color: ColorSourceApp.black,
-          width: 1.5,
-        ),
-      ),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          border: Border.all(
-            color: ColorSourceApp.white,
-            width: 2.5,
-          ),
-        ),
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            border: Border.all(
-              color: ColorSourceApp.black,
-              width: 1.0,
-            ),
-          ),
-          child: Container(
+    return BlocProvider<AddItemToBusketCubit>(
+      create: (context) => AddItemToBusketCubit(),
+      child: BlocBuilder<AddItemToBusketCubit, AddItemToBusketState>(
+        builder: (context, state) {
+          return Container(
+            height: 45,
+            width: double.infinity,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
-              color: Colors.amber,
-            ),
-            child: TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MenuPageMain()),
-                );
-                debugPrint('Received click');
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.shopping_cart_outlined,
-                    color: ColorSourceApp.black,
-                    size: 17,
-                  ),
-                  const SizedBox(
-                    width: 3,
-                  ),
-                  const Text('Add to Card ',
-                      style: TextStyle(color: ColorSourceApp.black)),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Text('\$$totalPrice',
-                      style: const TextStyle(color: ColorSourceApp.black)),
-                ],
+              border: Border.all(
+                color: ColorSourceApp.black,
+                width: 1.5,
               ),
             ),
-          ),
-        ),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(
+                  color: ColorSourceApp.white,
+                  width: 2.5,
+                ),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(
+                    color: ColorSourceApp.black,
+                    width: 1.0,
+                  ),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.amber,
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      // context.read<AddItemToBusketCubit>().addToCart();
+                      // debugPrint('Received click');
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Icons.shopping_cart_outlined,
+                          color: ColorSourceApp.black,
+                          size: 17,
+                        ),
+                        const SizedBox(
+                          width: 3,
+                        ),
+                        const Text('Add to Card ',
+                            style: TextStyle(color: ColorSourceApp.black)),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text('\$$totalPrice',
+                            style:
+                                const TextStyle(color: ColorSourceApp.black)),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
