b938be4cf7d39f62910a8a804762bf111c5a2989
viirgess <viirgess@gmail.com>
2023-09-04 19:33:14 +0300

    added location map

--- lib/menu/components/menu_drawer/drawer.dart
+++ lib/menu/components/menu_drawer/drawer.dart
@@ -1,10 +1,9 @@
-import 'package:app/cubit/register/register_page_cubit.dart';
+import 'package:app/menu/components/menu_drawer/component/profile/profile.dart';
 import 'package:app/utils/color_source.dart';
 import 'package:app/utils/text_style.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'component/orders/order_page.dart';
 
